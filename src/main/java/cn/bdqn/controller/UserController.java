095ceb8af5557a471fd54d3e011af66fe53d9a75
fengjun0328 <783585655@qq.com>
2018-12-29 17:49:33 +0800

    登录的方法

--- src/main/java/cn/bdqn/controller/UserController.java
+++ src/main/java/cn/bdqn/controller/UserController.java
@@ -2,6 +2,7 @@ package cn.bdqn.controller;
 
 import cn.bdqn.pojo.User;
 import cn.bdqn.service.UserService;
+import org.apache.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,21 +16,27 @@ import javax.servlet.http.HttpSession;
 @Controller
 @RequestMapping("/user")
 public class UserController {
+    private static Logger logger=Logger.getLogger(UserController.class);
     @Resource
     private UserService userService;
 
-    @RequestMapping(value = "/index.hmtl",method = RequestMethod.POST)
+    @RequestMapping(value = "/login.hmtl",method = RequestMethod.POST)
     @ResponseBody
     public Object index(@RequestParam  String loginCode, @RequestParam String password, HttpSession session){
+        logger.debug("==================>");
        int result=userService.getUser(loginCode,password);
        if(result==1){
            //还要修改登录时间
+           logger.debug("reslut:"+result);
             return "success";
        }else if(result==-1){
+           logger.debug("reslut:"+result);
            return "nologincode";
        }else if(result==-2){
+           logger.debug("reslut:"+result);
            return  "failed";
        }else if(result==0){
+           logger.debug("reslut:"+result);
            return "pwderror";
        }
        return "nodata";
@@ -40,8 +47,9 @@ public class UserController {
         return  "main";
     }
 
-    @RequestMapping("/login.html")
+    @RequestMapping(value = "/login.html",method = RequestMethod.GET)
     public String login(){
+        logger.debug("登录=====================》");
         return  "index";
     }
 }
